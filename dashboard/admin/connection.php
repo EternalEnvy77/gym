7022180d7f03fe38a8df3cc8abd9193a557d2174
suneet <suneet133@gmail.com>
2018-05-02 15:31:04 +0530

    configure database with password

--- dashboard/admin/connection.php
+++ dashboard/admin/connection.php
@@ -1,7 +1,7 @@
 <?php
 $host         = "localhost";
 $user         = "root";
-$password     = "";
+$password     = "secret";
 $databasename = "dbgym";
 
 $con = mysqli_connect($host, $user, $password, $databasename);
--- dashboard/admin/db_conn.php
+++ dashboard/admin/db_conn.php
@@ -1,7 +1,7 @@
 <?php
 $host     = "localhost"; // Host name 
 $username = "root"; // Mysql username 
-$password = ""; // Mysql password 
+$password = "secret"; // Mysql password 
 $db_name  = "dbgym"; // Database name 
 // Connect to server and select databse.
 $con      = mysqli_connect($host, $username, $password, $db_name);
